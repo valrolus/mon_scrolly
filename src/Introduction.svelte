4d460684eb4a1560fbacced59a334bd10f4b9821
valrolus <164334700+valrolus@users.noreply.github.com>
2024-04-19 15:03:05 +0200

    Update Introduction.svelte

--- src/Introduction.svelte
+++ src/Introduction.svelte
@@ -1,2 +1,3 @@
-<p>Malgré des signes de reprise encourageants, les cinémas belges n'ont pas encore renoué avec le niveau de fréquentation d'avant la pandémie de COVID-19 en 2022. Une analyse approfondie des données met en lumière une chute drastique de la fréquentation en 2020, principalement attribuée aux restrictions sanitaires imposées, suivie d'une reprise timide en 2021 et 2022.</p>
+<p>MalgrÃ© des signes de reprise encourageants, les cinÃ©mas belges n'ont pas encore renouÃ© avec le niveau de frÃ©quentation d'avant la pandÃ©mie de COVID-19 en 2022. Une analyse approfondie des donnÃ©es met en lumiÃ¨re une chute drastique de la frÃ©quentation en 2020, principalement attribuÃ©e aux restrictions sanitaires imposÃ©es, suivie d'une reprise timide en 2021 et 2022.</p>
+
 <p>Source: <em><a href="https://statbel.fgov.be/fr/themes/entreprises/cinemas#figures" target="_blank">Statbel</a></em></p>
